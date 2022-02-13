75107630c540aeff3a6a6dff24708ea49ee8f1bc
outercloudstudio <outercloudstudio@gmail.com>
2022-02-12 18:22:09 -0600

    Revert "Attempt At Structure"
    This reverts commit 1263905c3d92671fe27c8c0ab1f81198644e370a.

--- src/main/java/com/exdrill/ce/registry/ModGeneration.java
+++ src/main/java/com/exdrill/ce/registry/ModGeneration.java
@@ -1,10 +1,8 @@
 package com.exdrill.ce.registry;
 
 import com.exdrill.ce.Main;
-import com.exdrill.ce.world.features.MountainShrineFeature;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
-import net.fabricmc.fabric.api.structure.v1.FabricStructureBuilder;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
@@ -19,24 +17,11 @@ import java.util.List;
 public class ModGeneration {
     public static final ConfiguredFeature<?, ?> ORE_LIMESTONE = ConfiguredFeatures.register("ore_limestone", Feature.ORE.configure(new OreFeatureConfig(OreConfiguredFeatures.BASE_STONE_OVERWORLD, ModBlocks.LIGHTNING_ANCHOR.getDefaultState(), 10)));
 
-    private static final StructureFeature<DefaultFeatureConfig> MOUNTAIN_SHRINE_FEATURE = new MountainShrineFeature(DefaultFeatureConfig.CODEC);
-    private static final ConfiguredStructureFeature<?, ?> MOUNTAIN_SHRINE_CONFIGURED = MOUNTAIN_SHRINE_FEATURE.configure(DefaultFeatureConfig.DEFAULT);
-
     public static void registerFeatures(){
         RegistryKey<PlacedFeature> oreLimestoneLower = RegistryKey.of(Registry.PLACED_FEATURE_KEY,
                 new Identifier(Main.NAMESPACE, "ore_limestone_lower"));
         Registry.register(BuiltinRegistries.PLACED_FEATURE, oreLimestoneLower.getValue(), ORE_LIMESTONE.withPlacement(modifiersWithCount(200, HeightRangePlacementModifier.uniform(YOffset.fixed(-64), YOffset.fixed(256)))));
         BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, oreLimestoneLower);
-
-        FabricStructureBuilder.create(new Identifier("tutorial", "my_structure"), MOUNTAIN_SHRINE_FEATURE)
-                .step(GenerationStep.Feature.SURFACE_STRUCTURES)
-                .defaultConfig(32, 8, 12345)
-                .adjustsSurface()
-                .register();
-
-        RegistryKey<ConfiguredStructureFeature<?, ?>> mountainShrineConfigured = RegistryKey.of(Registry.CONFIGURED_STRUCTURE_FEATURE_KEY,
-                new Identifier(Main.NAMESPACE, "mountain_shrine"));
-        BuiltinRegistries.add(BuiltinRegistries.CONFIGURED_STRUCTURE_FEATURE, mountainShrineConfigured.getValue(), MOUNTAIN_SHRINE_CONFIGURED);
 
         System.out.println("Registered Features");
     }
