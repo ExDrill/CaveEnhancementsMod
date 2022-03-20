73ce6d4a99362cd12469d5079adb8c8bbca18bb8
ExDrill <72329798+ExDrill@users.noreply.github.com>
2022-03-19 19:33:06 -0500

    Changes

--- src/main/java/com/exdrill/ce/client/model/entity/model/CruncherModel.java
+++ src/main/java/com/exdrill/ce/client/model/entity/model/CruncherModel.java
@@ -2,7 +2,10 @@ package com.exdrill.ce.client.model.entity.model;
 
 import com.exdrill.ce.entity.CruncherEntity;
 import net.minecraft.util.Identifier;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
 
 public class CruncherModel extends AnimatedGeoModel<CruncherEntity> {
     @Override
@@ -21,4 +24,17 @@ public class CruncherModel extends AnimatedGeoModel<CruncherEntity> {
     public Identifier getAnimationFileLocation(CruncherEntity object) {
         return new Identifier("ce", "animations/cruncher.animation.json");
     }
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @Override
+    public void setLivingAnimations(CruncherEntity entity, Integer uniqueID, AnimationEvent customPredicate) {
+        super.setLivingAnimations(entity, uniqueID, customPredicate);
+        IBone head = this.getAnimationProcessor().getBone("head");
+        EntityModelData extraData = (EntityModelData) customPredicate.getExtraDataOfType(EntityModelData.class).get(0);
+        if (head != null) {
+            head.setRotationX(extraData.headPitch * ((float) Math.PI / 360F));
+            head.setRotationY(extraData.netHeadYaw * ((float) Math.PI / 340F));
+        }
+    }
+
 }
