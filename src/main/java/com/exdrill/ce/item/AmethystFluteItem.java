89c08d848db2cd54d503029cd535352bba043fb1
ExDrill <72329798+ExDrill@users.noreply.github.com>
2022-03-21 16:23:28 -0500

    More fixes

--- src/main/java/com/exdrill/ce/item/AmethystFluteItem.java
+++ src/main/java/com/exdrill/ce/item/AmethystFluteItem.java
@@ -1,17 +1,10 @@
 package com.exdrill.ce.item;
 
-import net.minecraft.entity.Bucketable;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnReason;
-import net.minecraft.entity.passive.OcelotEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class AmethystFluteItem extends Item {
