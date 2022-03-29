a6ff96c025f3e87df143abfcd3a1459df80c46f9
ExDrill <72329798+ExDrill@users.noreply.github.com>
2022-03-29 15:19:01 -0500

    Update CaveEnhancements.java

--- src/main/java/com/exdrill/ce/CaveEnhancements.java
+++ src/main/java/com/exdrill/ce/CaveEnhancements.java
@@ -2,9 +2,8 @@ package com.exdrill.ce;
 
 import com.exdrill.ce.registry.*;
 import net.fabricmc.api.ModInitializer;
-import terrablender.api.TerraBlenderApi;
 
-public class CaveEnhancements implements ModInitializer, TerraBlenderApi {
+public class CaveEnhancements implements ModInitializer {
     public static final String NAMESPACE = "ce";
 
     @Override
@@ -18,12 +17,5 @@ public class CaveEnhancements implements ModInitializer, TerraBlenderApi {
         ModBiomes.registerBiomes();
         ModBiomes.registerBiomeModifications();
     }
-
-
-
-    @Override
-        public void onTerraBlenderInitialized() {
-            //Regions.register(new CustomBiomeProvider(new Identifier(NAMESPACE, "custom_biome_provider"), RegionType.OVERWORLD, 10));
-        }
-    }
+}
 
