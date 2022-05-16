830345e07c73fd2e0c1ef085a08cd6ad3bcf7b84
ExDrill <72329798+ExDrill@users.noreply.github.com>
2022-05-16 12:51:16 -0500

    Removal of GeckoLib

--- src/main/java/com/exdrill/ce/block/entity/RoseQuartzChimesBlockEntity.java
+++ src/main/java/com/exdrill/ce/block/entity/RoseQuartzChimesBlockEntity.java
@@ -14,13 +14,6 @@ import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.world.World;
-import software.bernie.geckolib3.core.IAnimatable;
-import software.bernie.geckolib3.core.PlayState;
-import software.bernie.geckolib3.core.builder.AnimationBuilder;
-import software.bernie.geckolib3.core.controller.AnimationController;
-import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
-import software.bernie.geckolib3.core.manager.AnimationData;
-import software.bernie.geckolib3.core.manager.AnimationFactory;
 
 import java.util.Iterator;
 import java.util.List;
